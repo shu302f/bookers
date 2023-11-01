e6a2732ad8046ed5405e8d8663e5d5990b95888f
EC2 Default User <ec2-user@ip-172-31-31-2.ap-northeast-1.compute.internal>
2023-11-01 14:08:40 +0000

    [Add] create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,22 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
   def show
   end
 
   def edit
   end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
